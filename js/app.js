0e8e3e1f79c82ed3b3d19c241a0ed367690ba9c0
yusufkayum197 <yusufkaym13@gmail.com>
2025-06-18 15:37:38 +0500

    first commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const hamburgerIcon = document.querySelector(".hamburger__icon");
+const closeIcon = document.querySelector(".close__icon");
+const header__list = document.querySelector(".header__list");
+
+hamburgerIcon.addEventListener('click', () =>{
+    closeIcon.style.display = "block";
+    header__list.style.display = "block" ;
+    hamburgerIcon.style.display = "none";
+});
+
+closeIcon.addEventListener('click', () =>{
+    closeIcon.style.display = "none";
+    header__list.style.display = "none";
+    hamburgerIcon.style.display = "block";
+});
